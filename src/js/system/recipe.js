0fd372cfc965cf69e7c58f2172936c1ff6e5c75e
Ronald Compracion <compracionron@gmail.com>
2023-12-29 04:42:14 +0800

    Added Comments Functionality.

--- src/js/system/recipe.js
+++ src/js/system/recipe.js
@@ -36,6 +36,7 @@ async function fetchAndDisplayRecipe() {
 
     // Populate the HTML structure with fetched recipe details
     populateRecipeDetails(recipeDetails);
+    await displayCommentsForRecipe(currentRecipeId);
   } catch (error) {
     console.error("Error fetching recipe:", error.message);
   }
@@ -235,3 +236,205 @@ function navigateToRecipePage(recipeId) {
   console.log(`Clicked Recipe ID: ${recipeId}`);
   // You can perform any action here based on the clicked recipe ID
 }
+
+//
+//
+// COMENTS SECTION
+// Function to handle form submission
+// Function to generate a signed URL and submit a comment with image to the database
+// Function to generate a signed URL and submit a comment with image to the database
+
+// Function to upload the image and get the signed URL
+async function uploadAndGetURL(file) {
+  try {
+    const { data: uploadData, error: uploadError } = await supabase.storage
+      .from("comments")
+      .upload(`images/${file.name}`, file);
+
+    if (uploadError) {
+      console.error("Error uploading file:", uploadError);
+      return null;
+    }
+
+    const imagePath = uploadData.path || uploadData.fullPath || undefined;
+    return imagePath;
+  } catch (err) {
+    console.error("Error:", err.message);
+    return null;
+  }
+}
+
+// Function to submit comment with image path and other values
+// Function to submit comment with image path and other values
+document
+  .getElementById("comments-form")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    await fetchAndDisplayRecipe();
+
+    const comment = document.getElementById("comment").value;
+    const fileInput = document.getElementById("file");
+    const rating = document.getElementById("rating").value;
+
+    // Check if a file is uploaded
+    if (fileInput.files.length > 0) {
+      const file = fileInput.files[0];
+
+      const imagePath = await uploadAndGetURL(file);
+
+      if (!imagePath) {
+        console.error("Error obtaining image path.");
+        return;
+      }
+
+      // Submit comment with image path and other values
+      await submitComment(comment, imagePath, rating, currentRecipeId);
+    } else {
+      // Submit comment without an image
+      await submitComment(comment, null, rating, currentRecipeId);
+    }
+
+    // Function to submit a comment
+    async function submitComment(comment, imagePath, rating, recipeId) {
+      try {
+        // Retrieve the email from localStorage
+        const userEmail = localStorage.getItem("email");
+
+        const { data, error } = await supabase.from("ratings").insert([
+          {
+            comments: comment,
+            image_path: imagePath,
+            rating: parseInt(rating),
+            recipe_id: recipeId,
+            email: userEmail, // Store the email in the "ratings" table
+          },
+        ]);
+
+        if (error) {
+          console.error("Error inserting data:", error);
+          // Display error alert
+          alert("Error inserting data");
+          return;
+        }
+
+        console.log("Data inserted successfully:", data);
+
+        // After inserting a new comment, refresh the comments section
+        await displayCommentsForRecipe(recipeId);
+
+        // Reset form
+        document.getElementById("comments-form").reset();
+
+        // Display success alert
+        alert("Comment submitted successfully");
+      } catch (err) {
+        console.error("Error:", err.message);
+        // Display error alert
+        alert("Error occurred while submitting comment");
+      }
+    }
+  });
+
+//
+//
+//
+// DISPLAY COMMENTS
+function generateStars(rating) {
+  const starSymbols = "⭐".repeat(rating); // Repeats the star symbol 'rating' times
+  return starSymbols;
+}
+
+// Function to fetch user info and display comments
+async function displayCommentsForRecipe(recipeId) {
+  try {
+    // Fetch ratings data from Supabase based on recipe_id
+    let { data: ratings, error: ratingsError } = await supabase
+      .from("ratings")
+      .select("*")
+      .eq("recipe_id", recipeId);
+
+    if (ratingsError) {
+      console.error("Error fetching ratings:", ratingsError);
+      return;
+    }
+
+    console.log("Ratings:", ratings);
+
+    // Display comments using retrieved data
+    const commentSection = document.getElementById("comment_section");
+    commentSection.innerHTML = "";
+    const supabaseStorageURL =
+      "https://yjzcvrbsaxmfuaeakpzl.supabase.co/storage/v1/object/public/comments/";
+
+    // Loop through each comment
+    for (const rating of ratings) {
+      const userEmail = rating.email; // Assuming the email is stored in the 'email' column of the 'ratings' table
+
+      // Fetch user_info data from Supabase based on the email associated with the rating
+      let { data: userInfo, error: userInfoError } = await supabase
+        .from("user_info")
+        .select("*")
+        .eq("email", userEmail);
+
+      if (userInfoError) {
+        console.error("Error fetching user info:", userInfoError);
+        continue; // Skip to the next iteration in case of an error
+      }
+
+      console.log("User Info:", userInfo);
+
+      if (!userInfo || userInfo.length === 0) {
+        console.error("User info not found or empty.");
+        continue; // Skip to the next iteration if user info is empty
+      }
+
+      const fullName = `${userInfo[0].firstname} ${userInfo[0].lastname}`;
+
+      // Create HTML structure for comment
+      const commentDiv = document.createElement("div");
+      commentDiv.classList.add("row", "pb-3");
+      const imagePath = rating.image_path
+        ? supabaseStorageURL + rating.image_path
+        : "";
+      // Construct the HTML content for comments using fetched data
+      commentDiv.innerHTML = `
+      <div class="row">
+        <div class="col-2">
+          <img
+            id="profile_image"
+            width="35px"
+            height="35px"
+            class="img-account-profile rounded-circle mb-2"
+            src="${userInfo[0].image_path}"
+            alt="Profile Picture"
+          />
+        </div>
+        <div class="col-10 pt-2">
+          <h6>
+            <b id="fullname">${fullName}</b><br />
+            <span><small id="stars">${generateStars(
+              rating.rating
+            )}</small></span>
+          </h6>
+        </div>
+      </div>
+      <p class="text-dark px-5" id="comment">${rating.comments}</p>
+      <img
+        class="px-5"
+        id="image_path"
+        src="${supabaseStorageURL + rating.image_path}"
+        width="70px"
+        alt=""
+      />
+    `;
+
+      // Append the comment div to the comment section
+      commentSection.appendChild(commentDiv);
+    }
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+displayCommentsForRecipe(recipeId);
